5838000f725d3ea8f16742d3dd5f787b7b17c7bf
OSAMA-1329 <138973567+OSAMA-1329@users.noreply.github.com>
2023-09-10 10:22:23 +0530

    Add files via upload

---
+++ qr.js
@@ -0,0 +1,6 @@
+const wrapper= document.querySelector(".wrapper"),
+qrInput = wrapper.querySelector(".form input"),
+generateBtn =wrapper.querySelector(".form button"),
+qrimg= wrapper.querySelector(".form button"),
+
+
